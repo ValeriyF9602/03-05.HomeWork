3a8004ad4da6b5e7e1c6af39566995955f3e0c82
Valeriy Fateev <Valerka9602@mail.ru>
2023-03-01 21:44:01 +0800

    Решение задачи 36

--- ex36/Program.cs
+++ ex36/Program.cs
@@ -10,3 +10,42 @@
 
 **********************************************************/
 
+
+int EnterNum(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int[length];
+    Random rand = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int SumOddPosElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int size = EnterNum("Укажите размер массива: ");
+int min = EnterNum("Укажите минимальный элемент массива: ");
+int max = EnterNum("Укажите максимальный элемент массива: ");
+var myArray = GenerateArray(size, min, max);
+PrintArray(myArray);
+Console.WriteLine($"Сумма элементов нечётного индекса: {SumOddPosElements(myArray)}");
